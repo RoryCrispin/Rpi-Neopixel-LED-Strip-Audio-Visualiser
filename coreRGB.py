812e7df8ef95255240286091fc92529c442d61c2
Rory Crispin <me@rorycrispin.co.uk>
2015-09-08 21:05:47 +0100

    first commit

---
+++ coreRGB.py
@@ -0,0 +1,90 @@
+class rgbColour(object):
+    red = 0
+    green = 0
+    blue = 0
+
+    def __init__(self, red, green, blue, safe_brightness=False):
+        # By default safe_brightness will block low duty cycles to keep fade
+        # motions smooth
+
+        if not safe_brightness:
+            self.red = red
+            self.green = green
+            self.blue = blue
+        else:
+            self.safe_brightnessF(red, green, blue, safe_brightness)
+
+    def with_brightness(self, multiplier):
+        return rgbColour(
+            self.red *
+            multiplier,
+            self.green *
+            multiplier,
+            self.blue *
+            multiplier)
+
+    def safe_brightnessF(self, red, green, blue, safe_b):
+        def set_min(val, minimum):
+            if val < minimum:
+                return minimum
+            else:
+                return val
+
+        self.red = set_min(red, safe_b)
+        self.blue = set_min(blue, safe_b)
+        self.green = set_min(green, safe_b)
+
+
+def hex_rgb_to_colour(r, g, b):
+    hex_constant = 0.3921568627
+    return rgbColour(int(hex_constant * r),
+                     int(hex_constant * g),
+                     int(hex_constant * b))
+
+
+def hex_to_colour(v):
+    if v[0] == '#':
+        v = v[1:]
+    assert (len(v) == 6)
+    # return int(v[:2], 16), int(v[2:4], 16), int(v[4:6], 16)
+    return hex_rgb_to_colour(int(v[:2], 16), int(v[2:4], 16), int(v[4:6], 16))
+
+
+def percentageToHex(percentage):
+    return 2.55 * percentage
+
+def hexToPercentage(hex):
+    return hex/255*100
+
+# Preset Colours
+red = rgbColour(100, 0, 0)
+green = rgbColour(0, 100, 0)
+blue = rgbColour(0, 0, 100)
+orange = hex_rgb_to_colour(255, 127, 0)
+yellow = hex_rgb_to_colour(255, 255, 0)
+indigo = hex_rgb_to_colour(75, 0, 130)
+lime = hex_rgb_to_colour(125,255,0)
+ocean = rgbColour(0,54,100)
+purple = rgbColour(100, 0, 100)
+violet = purple
+hot_pink = hex_rgb_to_colour(255,0,125)
+aqua = rgbColour(0, 100, 100)
+turquoise = rgbColour(0, 100, 30)
+
+namedColours = {
+    "red": red,
+    "green": green,
+    "blue": blue,
+    "orange": orange,
+    "yellow": yellow,
+    "indigo": indigo,
+    "lime": lime,
+    "ocean": ocean,
+    "purple": purple,
+    "violet":purple,
+    "hot pink": hot_pink,
+    "aqua": aqua,
+    "turquoise": turquoise
+}
+
+
---
+++ stripLib.py
@@ -0,0 +1,54 @@
+import time
+import _rpi_ws281x as ws
+from neopixel import *
+import coreRGB
+
+# LED strip configuration:
+LED_COUNT      = 300      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+# Create NeoPixel object with appropriate configuration.
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+	strip.begin()
+
+
+class ledStrip(object):
+    def __init__(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS):
+    self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    self.strip.begin()
+        
+    def output(self):
+        strip.show()
+
+class canvas(object):
+    def __init__(self):
+
+
+# LED strips exist as a chain of objects, from individual RGBLEDs>Groups of LEDs> Canvases> Physical strip
+# Canvases sit between LED groups and the physical LED strips - allowing for filters to be placed over led groups before outputting, eg hue transformations, kaleidoscopes, motions and so on.
+class ledGroup(object):
+    def __init__(self, groupid = 0):
+        
+        self.ledArray = [] 
+    def populate():
+        for i in LED_COUNT:
+            self.ledArray[i] = rgbLED(groupid)      
+
+class rgbLED(object):
+    def __init__(self, group):
+        self.colour = coreRGB.rgbColour(0,0,0)
+        self.group = group
+    
+    def setColour(self, color):
+        self.colour = color
+    
+    def turnOff(self):
+        self.colour = coreRGB.rgbColour(0,0,0)
+
+
